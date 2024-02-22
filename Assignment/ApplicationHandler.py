7aebf83dace9796b4899c2820559d279be73b3a0
mukund-sharma-hm <160141775+mukund-sharma-hm@users.noreply.github.com>
2024-02-22 20:21:42 +0530

    Update ApplicationHandler.py

--- Assignment/ApplicationHandler.py
+++ Assignment/ApplicationHandler.py
@@ -39,19 +39,16 @@ import psutil
 
 instance = []
 def monitor_app(process_name, max_instnace):
-    for p in psutil.process_iter(['pid','name']):
-        if p.info['name'] == process_name:
+    for process in psutil.process_iter(['pid','name']):
+        if process.info['name'] == process_name:
             instance.append(p)
 
     if len(instance) > max_instnace:
         print(f"Killing {process_name} (more than {max_instnace})")
-        # Kill the oldest process
         instance[0].kill()
-        print("Done.")
+        print("killed process")
     else:
         print(f"Number of instances of {process_name}: {len(instance)}")
-
-# Monitoring a single app with maximum 2 instances
 monitor_app('notepad.exe', 2)
 
         
